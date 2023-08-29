55be06f9a808a058923427513d6268a0fd458da4
HyunSik Ryu <hsryu123@naver.com>
2023-08-29 23:18:47 +0900

    logback, log4j

--- src/main/java/com/example/weather/service/DiaryService.java
+++ src/main/java/com/example/weather/service/DiaryService.java
@@ -1,5 +1,6 @@
 package com.example.weather.service;
 
+import com.example.weather.WeatherApplication;
 import com.example.weather.domain.DateWeather;
 import com.example.weather.domain.Diary;
 import com.example.weather.repository.DateWeatherRepository;
@@ -9,6 +10,8 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -33,6 +36,7 @@ public class DiaryService {
 
     private final DiaryRepository diaryRepository;
     private final DateWeatherRepository dateWeatherRepository;
+    private static final Logger logger = LoggerFactory.getLogger(WeatherApplication.class);
 
     @Scheduled(cron = "0 0 1 * * *")
     @Transactional
@@ -42,6 +46,7 @@ public class DiaryService {
 
     @Transactional(isolation = Isolation.SERIALIZABLE)
     public void createDiary(LocalDate date, String text) {
+        logger.info("started to create diary");
         //날씨 데이터 가져오기(db에 이미 저장된 데이터 가져오기)
         DateWeather dateWeather = getDateWeather(date);
 
@@ -51,6 +56,8 @@ public class DiaryService {
         nowDiary.setText(text);
 
         diaryRepository.save(nowDiary);
+        logger.info("end to create diary");
+
     }
 
     private DateWeather getDateWeather(LocalDate date) {
@@ -81,6 +88,7 @@ public class DiaryService {
 
     @Transactional(readOnly = true)
     public List<Diary> readDiary(LocalDate date) {
+        logger.debug("read diary");
         return diaryRepository.findAllByDate(date);
     }
 
